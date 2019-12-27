f90093dbd402bb6e6d28f788a1ebaace97c00cd5
Babak Chehraz <bchehraz@csumb.edu>
2019-12-26 19:24:57 -0800

    Added jest for react unit and snapshot testing

---
+++ src/components/__tests__/HabitStats/CalendarView.js
@@ -0,0 +1,13 @@
+import React from "react"
+import renderer from "react-test-renderer"
+
+import CalendarView from "../../HabitStats/CalendarView.js"
+
+describe("CalendarView", () => {
+  it("renders correctly", () => {
+    const tree = renderer
+      .create(<CalendarView />)
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
