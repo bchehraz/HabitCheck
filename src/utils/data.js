92565896a44e7c9397654a7c6f1f336b0e5899b1
Babak Chehraz <bchehraz@csumb.edu>
2020-01-08 23:16:06 -0800

    Added static data for testing purposes

---
+++ src/utils/data.js
@@ -0,0 +1,91 @@
+export const data = {
+  habits: {
+    unchecked: [
+      {
+        title: 'Work on HabitCheck',
+        startDate: '12/02/2019',
+        bestStreak: 0,
+        progress: [],
+      },
+      {
+        title: 'Quit Smoking',
+        startDate: '10/25/2019',
+        bestStreak: 40,
+        bestStreakDate: '12/03/2019',
+        progress: [{
+          date: "10/25/2019",
+          streak: 40
+        }]
+      },
+      {
+        title: 'Meditate',
+        startDate: '11/28/2019',
+        bestStreak: 1,
+        bestStreakDate: '11/28/2019',
+        progress: [
+          { date: "11/28/2019", streak: 1 },
+          { date: "11/29/2019", streak: -3 }
+        ]
+      },
+      {
+        title: 'Sleep on time',
+        startDate: '11/29/2019',
+        bestStreak: 1,
+        bestStreakDate: '11/30/2019',
+        progress: [
+          { date: "11/30/2019", streak: 1 },
+          { date: "12/1/2019", streak: -1 },
+        ]
+      },
+    ],
+    checked: [
+      {
+        title: '90 min Yoga',
+        startDate: '12/1/2019',
+        bestStreak: 1,
+        bestStreakDate: '12/1/2019',
+        progress: [{
+          date: "12/1/2019",
+          streak: 1,
+        }]
+      },
+      {
+        title: 'Push Ups',
+        startDate: '12/1/2019',
+        bestStreak: 1,
+        bestStreakDate: '12/1/2019',
+        progress: [{
+          date: "12/1/2019",
+          streak: 1,
+        }]
+      },
+      {
+        title: '30 min walk',
+        startDate: '12/1/2019',
+        bestStreak: 1,
+        bestStreakDate: '12/1/2019',
+        progress: [{
+          date: "12/1/2019",
+          streak: 1,
+        }]
+      },
+      {
+        title: 'Keto Diet',
+        startDate: "12/1/2019",
+        bestStreak: 3,
+        bestStreakDate: '12/3/2019',
+        progress: [{
+          date: "12/1/2019",
+          streak: 3,
+        }]
+      },
+    ],
+  },
+}
+
+export const data_1 = {
+  habits: {
+    checked: [],
+    unchecked: [],
+  },
+}
