561a6ac8b56bf104bfff9f390066493743a6a00f
Babak Chehraz <bchehraz@csumb.edu>
2020-01-09 11:54:09 -0800

    Update: made changes to the context to support new features

--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React from "react"
 
 const AuthContext = React.createContext({
   token: null,
   userId: null,
   email: null,
   tokenExpiration: null,
-  data: {},
+  data: {
+    habits: {
+      lastUpdate: null,
+      checked: [],
+      unchecked: [],
+      map: [],
+    },
+  },
+  preferences: {
+    darkMode: false,
+    xEffectView: false,
+    selected: 0,
+  },
   // eslint-disable-next-line
-  login: (token, userId, tokenExpiration, email, data) => {},
+  login: (token, userId, tokenExpiration, email, data, preferences) => {},
   logout: () => {},
-  newHabit: (habit) => {},
-});
+  newHabit: habit => {},
+  checkHabit: (habitIndex, callback) => {},
+  uncheckHabit: (habitIndex, callback) => {},
+  toggleDarkMode: () => {},
+  toggleXEffectView: () => {},
+})
 
-export const AuthProvider = AuthContext.Provider;
-export const AuthConsumer = AuthContext.Consumer;
-export default AuthContext;
+export const AuthProvider = AuthContext.Provider
+export const AuthConsumer = AuthContext.Consumer
+export default AuthContext
