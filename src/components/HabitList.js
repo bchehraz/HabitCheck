90267c8240796f74ccc3bdde71a92b28d9c66d65
Babak Chehraz <bchehraz@csumb.edu>
2020-01-15 17:14:44 -0800

    Wired up habitlist and outputting data correctly

--- src/components/HabitList.js
+++ src/components/HabitList.js
@@ -1,37 +1,61 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import React from "react"
+import PropTypes from "prop-types"
+import styled from "styled-components"
 
-import HabitListItem from './HabitListItem';
-import AddHabitButton from './AddHabitButton';
+import HabitListItem from "./HabitListItem"
+import AddHabitButton from "./AddHabitButton"
 
 const HabitListContainer = styled.div`
   width: 100%;
-`;
+`
 
-const HabitList = ({ habits, handleAddHabit }) => (
+const HabitList = ({ habits, handleAddHabit, checkHabit, uncheckHabit }) => (
   <HabitListContainer>
     <AddHabitButton handleAddHabit={handleAddHabit} />
-    {habits.map((habit, index) => {
+    {habits.unchecked.map((habit, index) => {
+      const { title } = habit
+      let streak = 0
+      if (habit.progress.length !== 0) {
+        const { length } = habit.progress
+        streak = habit.progress[length - 1].streak
+      }
       return (
         <HabitListItem
           key={index}
-          title={habit.title}
-          streak={habit.streak}
-          isNew={habit.isNew}
-          isChecked={habit.isChecked}
+          title={title}
+          streak={streak}
+          isNew={streak === 0}
+          isChecked={false}
           onClick={() => {
-            console.log(`Clicked on Habit: <${habit.title}>`)
+            checkHabit(index)
           }}
         />
-      );
+      )
+    })}
+    {habits.checked.map((habit, index) => {
+      const { length } = habit.progress
+      const { title, progress } = habit
+      const { streak } = progress[length - 1]
+
+      return (
+        <HabitListItem
+          key={index}
+          title={title}
+          streak={streak}
+          isNew={streak === 0}
+          isChecked={true}
+          onClick={() => {
+            uncheckHabit(index)
+          }}
+        />
+      )
     })}
   </HabitListContainer>
-);
+)
 
 HabitList.propTypes = {
-  habits: PropTypes.array.isRequired,
+  habits: PropTypes.object.isRequired,
   handleAddHabit: PropTypes.func.isRequired,
 }
 
-export default HabitList;
+export default HabitList
