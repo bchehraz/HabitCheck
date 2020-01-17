b844d6a8ac4e656ac433863257ff5d8e2878f1a0
Babak Chehraz <bchehraz@csumb.edu>
2020-01-16 18:57:36 -0800

    Added more tests for checked data

--- src/utils/__tests__/HabitStats/XEffectData.js
+++ src/utils/__tests__/HabitStats/XEffectData.js
@@ -155,55 +155,238 @@ const output = {
   ],
   checked: [
     // "90 min Yoga"
-    [[2, 2, 2, 2, 2]],
+    [
+      [
+        2,
+        2,
+        2,
+        2,
+        2,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+      ],
+    ],
     // "Push Ups"
-    [[2, 1, 1, 2, 2]],
+    [
+      [
+        2,
+        1,
+        1,
+        2,
+        2,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+      ],
+    ],
     // "30 min walk"
-    [[2, 2, 2, 1, 2]],
+    [
+      [
+        2,
+        2,
+        2,
+        1,
+        2,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+      ],
+    ],
     // "Keto Diet"
-    [[2, 2, 1, 2, 2]],
+    [
+      [
+        2,
+        2,
+        1,
+        2,
+        2,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+      ],
+    ],
     // Quit Sugar
+    [
+      [
+        2,
+        2,
+        1,
+        1,
+        1,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+      ],
+      [
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+      ],
+    ],
   ],
 }
 
 // getXEffectData takes 3 parameters.
 // --- Currently, the site uses maxSize of 25 by default
 const MAX_SIZE = 25
-let isChecked = false
+const checked = true
 
 describe("getXEffectData should work", () => {
   // Unchecked habit progress
   it("with 'Work on HabitCheck' progress as input", () => {
     expect(
-      getXEffectData(data.habits.unchecked[0].progress, isChecked, MAX_SIZE)
+      getXEffectData(data.habits.unchecked[0].progress, !checked, MAX_SIZE)
     ).toStrictEqual(output.unchecked[0])
   })
 
   it("with 'Quit Smoking' progress as input", () => {
     expect(
-      getXEffectData(data.habits.unchecked[1].progress, isChecked, MAX_SIZE)
+      getXEffectData(data.habits.unchecked[1].progress, !checked, MAX_SIZE)
     ).toStrictEqual(output.unchecked[1])
   })
 
   it("with 'Meditate' progress as input", () => {
     expect(
-      getXEffectData(data.habits.unchecked[2].progress, isChecked, MAX_SIZE)
+      getXEffectData(data.habits.unchecked[2].progress, !checked, MAX_SIZE)
     ).toStrictEqual(output.unchecked[2])
   })
 
   it("with 'Sleep on time' progress as input", () => {
     expect(
-      getXEffectData(data.habits.unchecked[3].progress, isChecked, MAX_SIZE)
+      getXEffectData(data.habits.unchecked[3].progress, !checked, MAX_SIZE)
     ).toStrictEqual(output.unchecked[3])
   })
 
-  //isChecked = true;
   // Checked habit progress
-  // it("with '' progress as input", () => {
-  //   expect(getXEffectData(
-  //     data.habits.unchecked[1].progress,
-  //     isChecked,
-  //     MAX_SIZE
-  //   )).toBe(output.unchecked[1]);
-  // })
+  it("with 'Push Ups' progress as input", () => {
+    expect(
+      getXEffectData(data.habits.checked[0].progress, checked, MAX_SIZE)
+    ).toStrictEqual(output.checked[0])
+  })
+
+  it("with '30 min walk' progress as input", () => {
+    expect(
+      getXEffectData(data.habits.checked[1].progress, checked, MAX_SIZE)
+    ).toStrictEqual(output.checked[1])
+  })
+
+  it("with 'Keto Diet' progress as input", () => {
+    expect(
+      getXEffectData(data.habits.checked[2].progress, checked, MAX_SIZE)
+    ).toStrictEqual(output.checked[2])
+  })
+
+  it("with 'Quit Sugar' progress as input", () => {
+    expect(
+      getXEffectData(data.habits.checked[3].progress, checked, MAX_SIZE)
+    ).toStrictEqual(output.checked[3])
+  })
 })
