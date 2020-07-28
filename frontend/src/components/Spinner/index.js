e6657355f6a2619ba8f3d7c02c819bdc21d54825
Babak Chehraz <bchehraz@csumb.edu>
2020-07-27 17:52:53 -0700

    [Feature] Added new Spinner component

--- frontend/src/components/Spinner/index.js
+++ frontend/src/components/Spinner/index.js
@@ -1,73 +1,12 @@
 import React from "react"
 import "./spinnerStyle.css"
 
-const Spinner = () => (
-  <div
-    style={{
-      display: "flex",
-      justifyContent: "center",
-      margin: "0 auto",
-    }}
-  >
-    <div className="loadingio-spinner-blocks-f09e995srb">
-      <div className="ldio-h7s8jpvqb6w">
-        <div
-          style={{
-            left: "38px",
-            top: "38px",
-            animationDelay: "0s",
-          }}
-        ></div>
-        <div
-          style={{
-            left: "80px",
-            top: "38px",
-            animationDelay: "0.125s",
-          }}
-        ></div>
-        <div
-          style={{
-            left: "122px",
-            top: "38px",
-            animationDelay: "0.25s",
-          }}
-        ></div>
-        <div
-          style={{
-            left: "38px",
-            top: "80px",
-            animationDelay: "0.875s",
-          }}
-        ></div>
-        <div
-          style={{
-            left: "122px",
-            top: "80px",
-            animationDelay: "0.375s",
-          }}
-        ></div>
-        <div
-          style={{
-            left: "38px",
-            top: "122px",
-            animationDelay: "0.75s",
-          }}
-        ></div>
-        <div
-          style={{
-            left: "80px",
-            top: "122px",
-            animationDelay: "0.625s",
-          }}
-        ></div>
-        <div
-          style={{
-            left: "122px",
-            top: "122px",
-            animationDelay: "0.5s",
-          }}
-        ></div>
-      </div>
+const Spinner = ({ style }) => (
+  <div className="loadingio-spinner-dual-ball-6ttlhp9e3l8" style={style}>
+    <div className="ldio-xvoczscdqhs">
+      <div></div>
+      <div></div>
+      <div></div>
     </div>
   </div>
 )
