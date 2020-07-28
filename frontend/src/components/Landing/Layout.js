9e6c78b8c390594ff89bc4e7a2e050eb8c10c740
Babak Chehraz <bchehraz@csumb.edu>
2020-07-27 20:00:20 -0700

    [Renaming] Header to Header2 to see if this fixes circleci not deploying

--- frontend/src/components/Landing/Layout.js
+++ frontend/src/components/Landing/Layout.js
@@ -9,7 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./Header"
+import Header from "./Header2"
 import Footer from "./Footer"
 import "./layout.css"
 import "../Layout/layout.scss"
